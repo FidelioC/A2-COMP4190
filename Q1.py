da27de04129617b3ce7275a4471671f2b2befc44
Fidelio Ciandy <fiocheah08@gmail.com>
2026-02-17 16:00:25 -0600

    q1 a2

---
+++ Q1.py
@@ -0,0 +1,65 @@
+def queens_puzzle(n) -> list[list[str]]:
+    board = [["."] * n for _ in range(n)]
+    queen_positions = set()
+    solutions = []
+
+    backtrack(0, n, queen_positions, board, solutions)
+
+    return solutions
+
+
+def backtrack(row, n, queen_positions, board, solutions):
+    if row == n:
+        solutions.append(["".join(row) for row in board])
+        return
+
+    for col in range(n):
+        if is_attacked((row, col), queen_positions):
+            continue
+
+        board[row][col] = "Q"
+        queen_positions.add((row, col))
+        backtrack(row + 1, n, queen_positions, board, solutions)
+        board[row][col] = "."
+        queen_positions.remove((row, col))
+
+
+def is_attacked(position, queen_positions):
+    for queen in queen_positions:
+        if determine_queen_attack(position, queen):
+            return True
+    return False
+
+
+def determine_queen_attack(position_one: tuple, position_two: tuple) -> bool:
+    """Determine if two queens can attack each other
+
+    Args:
+        position_one (tuple): first queen position
+        position_two (tuple): second queen position
+    """
+    row_one, col_one = position_one
+    row_two, col_two = position_two
+
+    # row
+    if row_one == row_two:
+        return True
+
+    # col
+    if col_one == col_two:
+        return True
+
+    # diag
+    if abs(row_one - row_two) == abs(col_one - col_two):
+        return True
+
+    return False
+
+
+def main():
+    solutions = queens_puzzle(4)
+    print(solutions)
+
+
+if __name__ == "__main__":
+    main()
